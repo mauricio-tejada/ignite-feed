83debd4df5cc2269fa753d44b76c4e64a09f5446
Mauricio <you@example.com>
2022-10-18 23:08:24 -0300

    README

---
+++ README-PTBR.md
@@ -0,0 +1,50 @@
+# Ignite Feed
+
+Esta aplicação é um feed de postagens simples, com opções de comentar e reagir a comentários, criada para praticar os fundamentos do React e TypeScript.
+<img src="./public/screenshot.png">
+<br>
+<br>
+## 🚀 Começando
+
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+
+
+### 📋 Pré-requisitos
+
+
+```
+Node.JS instalado
+```
+
+### 🔧 Instalação
+
+Primeiramente faça o download do projeto em sua máquina.
+
+Instale as dependencias:
+
+```
+npm install
+```
+
+Para rodar a aplicação:
+
+```
+npm run dev
+```
+
+Após isso, siga o endereço informado no seu navegador, ex: http://localhost:5173/
+
+<br>
+<br>
+## 🛠️ Construído com
+
+* React
+* TypeScript
+* Vite
+* Styled Components
+
+<br>
+<br>
+---
+⌨️ com ❤️ por [Mauricio Fernandes Tejada](https://github.com/mauricio-tejada) 😊
--- src/Components/Main/main.tsx
+++ src/Components/Main/main.tsx
@@ -6,6 +6,20 @@ const posts = [
     {
         id: 1,
         author: {
+            avatarUrl: "https://github.com/diego3g.png",
+            name: "Diego Fernandes",
+            role: "CTO @Rocketseat",
+        },
+        content: [
+            { type: "paragraph", content: "Fala galeraa 👋" },
+            { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+            { type: "link", content: "jane.design/doctorcare" },
+        ],
+        publishedAt: new Date("2022-10-18 12:00:00")
+    },
+    {
+        id: 2,
+        author: {
             avatarUrl: "https://github.com/mauricio-tejada.png",
             name: "Mauricio Fernandes",
             role: "Frontend Dev",
@@ -15,21 +29,7 @@ const posts = [
             { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
             { type: "link", content: "jane.design/doctorcare" },
         ],
-        publishedAt: new Date("2022-10-09 12:00:00")
-    },
-    {
-        id: 2,
-        author: {
-            avatarUrl: "http://",
-            name: "Mauricio Fernandes",
-            role: "Frontend Dev",
-        },
-        content: [
-            { type: "paragraph", content: "Fala galeraa 👋" },
-            { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
-            { type: "link", content: "jane.design/doctorcare" },
-        ],
-        publishedAt: new Date("2022-10-09 12:00:00")
+        publishedAt: new Date("2022-10-17 12:00:00")
     }
 ]
 
